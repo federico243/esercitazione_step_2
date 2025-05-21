21732401b472bc0d3543245340d9daccd18f7c74
federico243 <federico.mbiasioli@gmail.com>
2025-05-21 11:04:42 +0200

    Corretto errore su controllo funzioni CheckValueInt/Double (problema con i numeri negativi risolto)

--- Menu.cpp
+++ Menu.cpp
@@ -49,7 +49,7 @@ void Menu::VisualizzaFunzioni() const
  */
 void Menu::InserisciFunzione()
 {
-    string stdgrado;     //Inizio variabili controllo dell'input
+    string stdgrado; // Inizio variabili controllo dell'input
     string stdconferma;
     string stdpol_coeff;
     string stdexp;
@@ -58,15 +58,15 @@ void Menu::InserisciFunzione()
     string stdlog_coeff;
     string stdscelta;
     const char *stdscelta2;
-    const char *stdconferma2; //Fine variabili controllo dell'input
+    const char *stdconferma2; // Fine variabili controllo dell'input
     int scelta = 1;
     int result = -1;
     int conferma = -1;
-    //oggetto per creare una copia iniziale della funzione
+    // oggetto per creare una copia iniziale della funzione
     Function *nuova = nullptr;
     do
     {
-        do //Scelta della funzione con controllo input
+        do // Scelta della funzione con controllo input
         {
             cout << "Che tipo di funzione vuoi inserire?" << endl;
             cout << "0 - Uscita dall'inserimento" << endl;
@@ -88,10 +88,10 @@ void Menu::InserisciFunzione()
         }
         case 1:
         {
-            const char *stdgrado2;  //Variabile di controllo input
-            const char *stdpol_coeff2; //Variabile di controllo input
+            const char *stdgrado2;     // Variabile di controllo input
+            const char *stdpol_coeff2; // Variabile di controllo input
             int grado;
-            do //Inserimento e controllo del grado di polinomio
+            do // Inserimento e controllo del grado di polinomio
             {
                 cout << "Grado del polinomio: " << endl;
                 getline(cin, stdgrado);
@@ -102,7 +102,7 @@ void Menu::InserisciFunzione()
             double *pol_coeff = new double[grado + 1];
             for (int i = 0; i <= grado; i++)
             {
-                do //Inserimento coefficienti con controllo dell'input
+                do // Inserimento coefficienti con controllo dell'input
                 {
                     cout << "Coefficiente di x^" << i << ": " << endl;
                     getline(cin, stdpol_coeff);
@@ -111,11 +111,11 @@ void Menu::InserisciFunzione()
                 } while (result == -1);
                 pol_coeff[i] = atof(stdpol_coeff2);
             }
-            //Creazione del polinomio
+            // Creazione del polinomio
             nuova = new Polinomial(pol_coeff, grado + 1);
             cout << "Hai inserito:" << endl;
             nuova->Dump();
-            do //Conferma di creazione più controllo input
+            do // Conferma di creazione più controllo input
             {
                 conferma = -1;
                 cout << "Confermi?(1=Si / 0=No):" << endl;
@@ -124,7 +124,7 @@ void Menu::InserisciFunzione()
                 if (CheckValueInt(stdconferma) == 1)
                     conferma = atoi(stdconferma2);
             } while (conferma != 1 && conferma != 0);
-            if (conferma) //aggiunta della nuova funzione nella lista
+            if (conferma) // aggiunta della nuova funzione nella lista
             {
                 functions.push_back(nuova);
                 cout << "Funzione registrata." << endl;
@@ -139,10 +139,10 @@ void Menu::InserisciFunzione()
         }
         case 2:
         {
-            const char *stdpow_coeff2;  //Variabile per il controllo dell'input
-            const char *stdexp2; //Variabile per il controllo dell'inputs
+            const char *stdpow_coeff2; // Variabile per il controllo dell'input
+            const char *stdexp2;       // Variabile per il controllo dell'inputs
             double pow_coeff, exp;
-            do //Inserimento coefficiente con controllo dell'input
+            do // Inserimento coefficiente con controllo dell'input
             {
                 cout << "Inserisci coefficiente: " << endl;
                 getline(cin, stdpow_coeff);
@@ -150,7 +150,7 @@ void Menu::InserisciFunzione()
                 result = CheckValueDouble(stdpow_coeff);
             } while (result == -1);
             pow_coeff = atof(stdpow_coeff2);
-            do //Inserimento esponente con controllo dell'input
+            do // Inserimento esponente con controllo dell'input
             {
                 cout << "Inserisci esponente: " << endl;
                 getline(cin, stdexp);
@@ -158,11 +158,11 @@ void Menu::InserisciFunzione()
                 result = CheckValueDouble(stdexp);
             } while (result == -1);
             exp = atof(stdexp2);
-            //Creazione della potenza
+            // Creazione della potenza
             nuova = new Power(pow_coeff, exp);
             cout << "Hai inserito:" << endl;
             nuova->Dump();
-            do //Conferma della creazione con controllo su input
+            do // Conferma della creazione con controllo su input
             {
                 conferma = -1;
                 cout << "Confermi?(1=Si / 0=No):" << endl;
@@ -172,7 +172,7 @@ void Menu::InserisciFunzione()
                     conferma = atoi(stdconferma2);
             } while (conferma != 1 && conferma != 0);
             if (conferma)
-            { //Inserimento nella lista della nuova funzione
+            { // Inserimento nella lista della nuova funzione
                 functions.push_back(nuova);
                 cout << "Funzione registrata." << endl;
             }
@@ -186,9 +186,9 @@ void Menu::InserisciFunzione()
         case 3:
         {
             double base, log_coeff;
-            const char *stdbase2; //Variabile per il controllo dell'input
-            const char *stdlog_coeff2; //Variabile per il controllo dell'input
-            do //Inserimento della base con controllo dell'input
+            const char *stdbase2;      // Variabile per il controllo dell'input
+            const char *stdlog_coeff2; // Variabile per il controllo dell'input
+            do                         // Inserimento della base con controllo dell'input
             {
                 cout << "Inserisci la base del logaritmo (diversa da 1 e > 0): " << endl;
                 getline(cin, stdbase);
@@ -196,7 +196,7 @@ void Menu::InserisciFunzione()
                 result = CheckValueDouble(stdbase);
             } while (result == -1);
             base = atof(stdbase2);
-            do //Inserimento coefficiente con controllo dell'input
+            do // Inserimento coefficiente con controllo dell'input
             {
                 cout << "Inserisci coefficiente: ";
                 getline(cin, stdlog_coeff);
@@ -204,11 +204,11 @@ void Menu::InserisciFunzione()
                 result = CheckValueDouble(stdlog_coeff);
             } while (result == -1);
             log_coeff = atof(stdlog_coeff2);
-            //Creazione del nuovo logaritmo
+            // Creazione del nuovo logaritmo
             nuova = new Logarithmic(base, log_coeff);
             cout << "Hai inserito:" << endl;
             nuova->Dump();
-            do //Salvataggio della nuova funzione con controllo
+            do // Salvataggio della nuova funzione con controllo
             {
                 conferma = -1;
                 cout << "Confermi?(1=Si / 0=No):" << endl;
@@ -218,7 +218,7 @@ void Menu::InserisciFunzione()
                     conferma = atoi(stdconferma2);
             } while (conferma != 1 && conferma != 0);
             if (conferma)
-            { //Copia della nuova funzione.
+            { // Copia della nuova funzione.
                 functions.push_back(nuova);
                 cout << "Funzione registrata." << endl;
             }
@@ -242,13 +242,13 @@ void Menu::InserisciFunzione()
  */
 void Menu::EliminaFunzione(int id)
 {
-    string stdid;  //Inizio variabili di controllo dell'input
+    string stdid; // Inizio variabili di controllo dell'input
     string stdconferma;
     const char *stdconferma2;
-    const char *stdid2;  //Fine variabili di controllo dell'input
+    const char *stdid2; // Fine variabili di controllo dell'input
     int controllo = 0;
     int conferma;
-    do //Controllo di id
+    do // Controllo di id
     {
         if (id < 0 || id >= (int)functions.size())
         {
@@ -267,7 +267,7 @@ void Menu::EliminaFunzione(int id)
     } while (controllo == 0);
     cout << "Funzione selezionata:" << endl;
     functions[id]->Dump();
-    do //Controllo di conferma
+    do // Controllo di conferma
     {
         conferma = -1;
         cout << "Confermi?(1=Si / 0=No):" << endl;
@@ -303,48 +303,58 @@ void Menu::EliminaTutte()
     cout << "Tutte le funzioni sono state eliminate." << endl;
 }
 
-/** 
-* @brief Check the value passed by the user (for int type)
-* @param cstring string to check
-* @return 1 if the string is a int, -1 if it's not
-*/
+/**
+ * @brief Check the value passed by the user (for int type)
+ * @param cstring string to check
+ * @return 1 if the string is a int, -1 if it's not
+ */
 int Menu::CheckValueInt(string cstring)
 {
-    if(cstring=="")
+    int counter = 0;
+    if (cstring == "")
     {
         cout << "Valore inserito errato" << endl;
         return -1;
     }
     for (char c : cstring)
     {
-        if (!isdigit(c))
+        if (counter != 0 && c == '-')
         {
-            cout << "Valore inserito errato" << endl;
-            return -1;
+            if (!isdigit(c))
+            {
+                cout << "Valore inserito errato" << endl;
+                return -1;
+            }
         }
+        counter++;
     }
     return 1;
 }
 
-/** 
-* @brief Check the value passed by the user (for double type)
-* @param cstring string to check
-* @return 1 if the string is a double, -1 if it's not
-*/
+/**
+ * @brief Check the value passed by the user (for double type)
+ * @param cstring string to check
+ * @return 1 if the string is a double, -1 if it's not
+ */
 double Menu::CheckValueDouble(string cstring)
 {
-    if(cstring=="")
+    int counter = 0;
+    if (cstring == "")
     {
         cout << "Valore inserito errato" << endl;
         return -1;
     }
     for (char c : cstring)
     {
-        if (!isdigit(c) && c != '.')
+        if (counter != 0 && c == '-')
         {
-            cout << "Valore inserito errato" << endl;
-            return -1;
+            if (!isdigit(c) && c != '.')
+            {
+                cout << "Valore inserito errato" << endl;
+                return -1;
+            }
         }
+        counter++;
     }
     return 1;
 }
@@ -358,15 +368,15 @@ double Menu::CheckValueDouble(string cstring)
 void Menu::SelezionaFunzione()
 {
     VisualizzaFunzioni();
-    int id;                    //Inizio ariabili per i controlli sulle stringhe
+    int id; // Inizio ariabili per i controlli sulle stringhe
     double x;
     string stdid;
     const char *stdid2;
     string stdx;
-    const char *stdx2;         //Fine variabili per i controlli sulle stringhe
+    const char *stdx2; // Fine variabili per i controlli sulle stringhe
     int controllo;
     int result = -1;
-    do //Controllo ID
+    do // Controllo ID
     {
         cout << "Inserisci ID della funzione: " << endl;
         getline(cin, stdid);
@@ -383,9 +393,9 @@ void Menu::SelezionaFunzione()
         else
             controllo = -1;
     } while (controllo == 0);
-    //Stampa della funzione scelta
+    // Stampa della funzione scelta
     functions[id]->Dump();
-    do //Controllo valore inserito
+    do // Controllo valore inserito
     {
         cout << "Inserisci valore di x: " << endl;
         getline(cin, stdx);
@@ -404,15 +414,15 @@ void Menu::SelezionaFunzione()
  */
 void Menu::MenuPrincipale()
 {
-    string stdscelta;          //Inizio variabili di controllo stringhe
+    string stdscelta; // Inizio variabili di controllo stringhe
     const char *stdscelta2;
     string stdid;
-    const char *stdid2;        //Fine variabili di controllo stringhe
+    const char *stdid2; // Fine variabili di controllo stringhe
     int scelta = -1;
     int result = -1;
     do
     {
-        do //Scelta iniziale con controllo su input
+        do // Scelta iniziale con controllo su input
         {
             cout << "========== MENU PRINCIPALE ==========" << endl;
             cout << "0  Esci dal menu" << endl;
@@ -421,12 +431,12 @@ void Menu::MenuPrincipale()
             cout << "3  Elimina una funzione" << endl;
             cout << "4  Elimina tutte le funzioni" << endl;
             cout << "5  Seleziona una funzione" << endl;
-            cout << "Scelta: "<< endl;
+            cout << "Scelta: " << endl;
             getline(cin, stdscelta);
             stdscelta2 = stdscelta.c_str();
             result = CheckValueInt(stdscelta);
         } while (result == -1);
-        scelta=atoi(stdscelta2);
+        scelta = atoi(stdscelta2);
         switch (scelta)
         {
         case 0:
@@ -449,7 +459,7 @@ void Menu::MenuPrincipale()
             else
             {
                 VisualizzaFunzioni();
-                do //Inserimento primo id con controllo dell'input 
+                do // Inserimento primo id con controllo dell'input
                 {
                     cout << "Inserisci ID della funzione: " << endl;
                     getline(cin, stdid);
